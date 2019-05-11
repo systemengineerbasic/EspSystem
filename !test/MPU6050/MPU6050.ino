8f04d3ef13f61b9ee90c4e02075cbcdc77203ca2
nishimura <yushi.nishimura@konicaminolta.com>
2019-05-11 11:31:18 +0900

    MPU-6050のデータをすべて表示

--- !test/MPU6050/MPU6050.ino
+++ !test/MPU6050/MPU6050.ino
@@ -30,40 +30,63 @@ void setup()
 
 }
 
-float pre_acc = 0;
+float pre_axl = 0;
 void loop() 
 {
 	int error;
-	float	acc_x, acc_y, acc_z;
+	float	axl_x, axl_y, axl_z;
 	float	gyro_x, gyro_y, gyro_z;
 	float	temperature;
 
 	// 加速度、角速度、温度を取得
-	error = MPU6050_get_all(&acc_x, &acc_y, &acc_z, &gyro_x, &gyro_y, &gyro_z, &temperature);
-
-	Serial.print(error, DEC);
-	Serial.print("\t");
-
-	Serial.print(temperature, 1);
-	Serial.print("\t");
-
-	float abs_acc2 = (acc_x*acc_x + acc_y*acc_y + acc_z*acc_z);
-	float diff_acc = abs_acc2 - pre_acc;
-	if(diff_acc < 0) {
-		diff_acc = -diff_acc;
-	}
-	pre_acc = abs_acc2;
-	Serial.print(diff_acc, 2);
-	Serial.print("\t");
-	Serial.println("");
-
-	if(diff_acc < 0.2) {
-		digitalWrite(IO_PIN_LED,LOW);
+	error = MPU6050_get_all(&axl_x, &axl_y, &axl_z, &gyro_x, &gyro_y, &gyro_z, &temperature);
+	if(error != 0) {
+		Serial.println("[Error] cannot get sensor data.");
 	}
 	else {
-		digitalWrite(IO_PIN_LED,HIGH);
+		Serial.print("Temp. =\t");
+		// 温度
+		Serial.print(temperature);
+		Serial.print("\t||\t");
+		// 加速度
+		Serial.print("Acc. =\t");
+		Serial.print(axl_x);
+		Serial.print("\t");
+		Serial.print(axl_y);
+		Serial.print("\t");
+		Serial.print(axl_z);
+		Serial.print("\t||\t");
+		// Gyro
+		Serial.print("Gyro. = \t");
+		Serial.print(gyro_x);
+		Serial.print("\t");
+		Serial.print(gyro_y);
+		Serial.print("\t");
+		Serial.print(gyro_z);
+
+/*
+		float abs_axl2 = (axl_x*axl_x + axl_y*axl_y + axl_z*axl_z);
+		float diff_axl = abs_axl2 - pre_axl;
+		if(diff_axl < 0) {
+			diff_axl = -diff_axl;
+		}
+		pre_axl = abs_axl2;
+		Serial.print(diff_axl, 2);
+		Serial.print("\t");
+		Serial.println("");
+
+		if(diff_axl < 0.2) {
+			digitalWrite(IO_PIN_LED,LOW);
+		}
+		else {
+			digitalWrite(IO_PIN_LED,HIGH);
+		}
+*/
+
+		Serial.println();
 	}
 
-	delay(50);
+	delay(1000);
+//	delay(50);
 }
 
