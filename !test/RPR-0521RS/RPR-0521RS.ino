8a1119dc671310d6cd2063fb0acfd8385cb96c14
nishimura <yushi.nishimura@konicaminolta.com>
2019-05-11 12:42:14 +0900

    コード整理

--- !test/RPR-0521RS/RPR-0521RS.ino
+++ !test/RPR-0521RS/RPR-0521RS.ino
@@ -26,49 +26,39 @@ RPR0521RS rpr0521rs;
 #define PIN_SCL SCL
 
 
-void setup() {
-  byte rc;
+void setup() 
+{
+	byte rc;
 
-  Serial.begin(115200);		//  Modify : set the serial speed to 11500 kbps
-  Serial.print("[1]");
-  while (!Serial);
-  Serial.print("[2]");
-  
-  Wire.begin(PIN_SDA, PIN_SCL);
-  Serial.print("[3]");
-  
-  rc = rpr0521rs.init();
-  Serial.print("[4]");
+	Serial.begin(9600);
+	while (!Serial);
 
+	Wire.begin(PIN_SDA, PIN_SCL);
+
+	rc = rpr0521rs.init();
 }
 
-void loop() {
+void loop() 
+{
+	int error;
+	unsigned short ps_val;
+	float als_val;
 
-  byte rc;
-  unsigned short ps_val;
-  float als_val;
-  byte near_far;
-  
-  rc = rpr0521rs.get_psalsval(&ps_val, &als_val);
-  if (rc == 0) {
-    Serial.print(F("RPR-0521RS (Proximity)     = "));
-    Serial.print(ps_val);
-    Serial.print(F(" [count]"));
-    near_far = rpr0521rs.check_near_far(ps_val);
-    if (near_far == RPR0521RS_NEAR_VAL) {
-      Serial.println(F(" Near"));
-    } else {
-      Serial.println(F(" Far"));
-    }
-    
-    if (als_val != RPR0521RS_ERROR) {
-      Serial.print(F("RPR-0521RS (Ambient Light) = "));
-      Serial.print(als_val);
-      Serial.println(F(" [lx]"));
-      Serial.println();
-    }
-  }
- 
-  delay(500);
+	error = rpr0521rs.get_psalsval(&ps_val, &als_val);
+	if(error == 0) {
+		// 近接センサー
+		Serial.print("Prox. = ");
+		Serial.print(ps_val);
+		Serial.print("\t||\t");
+		// 照度センサー
+		Serial.print("Bright. = ");
+		Serial.print(als_val);
 
+		Serial.println();
+	}
+	else {
+		Serial.println("[Error] cannot get sensor data.");
+	}
+
+	delay(1000);
 }
