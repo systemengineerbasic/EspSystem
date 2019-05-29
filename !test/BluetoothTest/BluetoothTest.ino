03d9ff99d419503c669183a249e724a7b2907271
nishimura <yushi.nishimura@konicaminolta.com>
2019-05-29 15:50:42 +0900

    ターミナルからの文字入力対応

--- !test/BluetoothTest/BluetoothTest.ino
+++ !test/BluetoothTest/BluetoothTest.ino
@@ -4,11 +4,25 @@ BluetoothSerial SerialBT;
 
 void setup()
 {
+	Serial.begin(9600);
 	SerialBT.begin("ESP32");
 }
 
+int count = 0;
 void loop()
 {
-	SerialBT.println("Hello World");
-	delay(1000);
+	if (Serial.available()) {
+		SerialBT.write(Serial.read());
+	}
+	if (SerialBT.available()) {
+		Serial.write(SerialBT.read());
+	}
+
+	if(count > 50) {
+		count = 0;
+		SerialBT.println("[Bluetooth] Hello World");
+	}
+	count ++;
+
+	delay(20);
 }
