87018343574de0a64b09ae4db989b6dc598cd8ca
nishimura <yushi.nishimura@konicaminolta.com>
2019-05-29 20:34:03 +0900

    LED & SW

--- !test/GpioTest/GpioTest.ino
+++ !test/GpioTest/GpioTest.ino
@@ -1,25 +1,76 @@
-#define READ_START 10
-#define READ_END 30
+#define	PIN_LED		(2)
+#define	PIN_SW		(0)
 
-void setup() {
 
-	int pin=0;
-	for(pin=READ_START; pin <= READ_END; pin++) {
-		pinMode(pin, INPUT);     // Initialize the LED_BUILTIN pin as an output
-	}
-
-	Serial.begin(115200);
+//=====================================
+// Initialize LED
+//=====================================
+void init_led()
+{
+    pinMode(PIN_LED, OUTPUT);
+    set_led_status(0);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-	int pin=0;
-	int rd;
-	for(pin=READ_START; pin <= READ_END; pin++) {
-		rd = digitalRead(pin);
-		Serial.printf("[%d] %d \n",pin, rd);
-		
-	}
-	  delay(2000);                      // Wait for a second
+//=====================================
+// Setup LED status(On or Off)
+//=====================================
+void set_led_status(int status) // status : 0->Off, not zero->On 
+{
+    if(status) { 
+        digitalWrite(PIN_LED, HIGH);    // turn on LED.
+    }
+    else {
+        digitalWrite(PIN_LED, LOW);     // turn off LED.
+    }
+}
+
+//=====================================
+// Initialize push switch
+//=====================================
+void init_sw()
+{
+    pinMode(PIN_SW, INPUT);
+}
+
+//=====================================
+// Get push switch status(On or Off)
+//=====================================
+int get_sw_status()
+{
+	int sw_status = digitalRead(PIN_SW);
+    if(sw_status == 0) { // If sw is pushed
+        return  1;
+    }
+    else {
+        return  0;
+    }
+}
+
+//=====================================
+// Initialization
+//=====================================
+void setup()
+{
+    // Initialize LED
+    init_led();
+    // Initialize push switch
+    init_sw();
+}
+
+//=====================================
+// Main loop
+//=====================================
+void loop() 
+{
+    int sw_status;
+	sw_status = get_sw_status();
+	
+    if(sw_status) { // If sw is pushed, turn on LED.
+        set_led_status(1);
+    }
+    else {
+        set_led_status(0);
+    }
+        
 }
 
