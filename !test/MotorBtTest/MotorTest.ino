2716ff5e30dd6afc68c2eb2291253533fa22089d
nishimura <yushi.nishimura@konicaminolta.com>
2019-06-10 15:44:05 +0900

    MotorTestをSerial通信版とBluetooth版に分けた

--- !test/MotorBtTest/MotorTest.ino
+++ !test/MotorBtTest/MotorTest.ino
@@ -20,6 +20,8 @@
 #define	MOTOR_DIR_REV				(2)
 
 
+BluetoothSerial SerialBT;
+
 int g_motor_speed = 180;
 
 void MOTOR_init()
@@ -135,14 +137,17 @@ void setup()
     // Initialize serial-port (115200bps)
     Serial.begin(115200);
 
+    // Initialize Bluetooth
+	SerialBT.begin("ESP32");
+
     // Initialize motor
     MOTOR_init();
 }
 
 void loop()
 {
-    if(Serial.available() > 0) { // received data
-        int getstr = Serial.read(); // Read data from serial-port
+    if(SerialBT.available() > 0) { // received data
+        int getstr = SerialBT.read(); // Read data from serial-port
         
         if(getstr == 's') {
         	MOTOR_set_speed_left(MOTOR_DIR_FWD, 0);
