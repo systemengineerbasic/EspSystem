d923cfbea6895104849b55b0c4cedb21ddacc3d2
nishimura <yushi.nishimura@konicaminolta.com>
2019-06-27 13:19:14 +0900

    センサーが動作する

--- !laboratory/LineTrace_lab_07/LineTrace_lab_07.ino
+++ !laboratory/LineTrace_lab_07/LineTrace_lab_07.ino
@@ -10,7 +10,8 @@
 #include "my_cmd.h"
 #include <Wire.h>
 #include <RPR-0521RS.h>
-#include "my_led_button.h"
+#include <MPU6050.h>
+#include <my_led_button.h>
 
 
 //---------------------- PIN ----------------------
@@ -27,6 +28,7 @@
 #define IO_PIN_LINETRACK_RIGHT  (39)
 // LED
 #define IO_PIN_LED_1            ( 2)
+#define IO_PIN_LED_2            (18)
 // Button
 #define IO_PIN_BUTTON_1         ( 0)
 
@@ -34,6 +36,8 @@
 #define DAC_CH_MOTOR_A          (0)
 #define DAC_CH_MOTOR_B          (1)
 #define DAC_CH_SERVO            (2)
+#define DAC_CH_LED_1            (3)
+#define DAC_CH_LED_2            (4)
 
 //---------------------- Trafic Signal ----------------------
 #define SIGNAL_COLOR_BLACK      (0)
@@ -123,13 +127,15 @@ int g_lr_level = 40;
 Stream* g_pSerial=&Serial; // Selected serial port (USB is default)
 
 SemaphoreHandle_t g_xMutex_Signal = NULL;   // for critical section for signal color
+SemaphoreHandle_t g_xMutex_Sensor = NULL;   // for critical section for reading sensor 
 
 RPR0521RS rpr0521rs;
+int g_mpu6050_enable  = 0; // if MPU-6050 is enabled
 int g_rpr0521rs_enable  = 0; // if RPR-0521RS is enabled
 float   g_brightness_thresh = 10.0; // Bright and dark threshold 
-
-my_led      g_led1(IO_PIN_LED_1);
-my_button   g_button1(IO_PIN_BUTTON_1);
+float   g_impact_thresh = 0.5; // Threshold for impact detection
+my_led      g_led1(IO_PIN_LED_1, DAC_CH_LED_1);
+my_led      g_led2(IO_PIN_LED_2, DAC_CH_LED_2);
 
 //===================================================================
 // MODULE   : min_max_hold()
@@ -210,6 +216,93 @@ void _cmd__signal(int argc, char* argv[])
     }
 }
 
+void _cmd__brightth(int argc, char* argv[])
+{
+    int error = 1;
+    if(argc > 1) {
+        float thresh = atof(argv[1]);
+        if(thresh > 0.0) {
+            g_brightness_thresh = thresh;
+        }
+        error = 0;
+    }
+    g_pSerial->print("current brightness thresh = ");
+    g_pSerial->println(g_brightness_thresh);
+    
+    if(error) {
+        char txt[128];
+        sprintf(txt, "[Usage] %s thresh(>0)", argv[0]);
+        g_pSerial->println(txt);
+    }
+}
+
+void _cmd__impactth(int argc, char* argv[])
+{
+    int error = 1;
+    if(argc > 1) {
+        float thresh = atof(argv[1]);
+        if(thresh > 0.0) {
+            g_impact_thresh = thresh;
+        }
+        error = 0;
+    }
+    g_pSerial->print("current impact thresh = ");
+    g_pSerial->println(g_impact_thresh);
+    
+    if(error) {
+        char txt[128];
+        sprintf(txt, "[Usage] %s thresh(>0)", argv[0]);
+        g_pSerial->println(txt);
+    }
+}
+
+void _cmd__sensor(int argc, char* argv[])
+{
+    int error;
+
+    if(g_mpu6050_enable) { // MPU-6050 was initialized
+    	float	axl_x, axl_y, axl_z;
+    	float	gyro_x, gyro_y, gyro_z;
+    	float	temperature;
+        xSemaphoreTake(g_xMutex_Sensor, portMAX_DELAY); // Å•Å•Å• Start critical section Å•Å•Å•
+		error = MPU6050_get_all(&axl_x, &axl_y, &axl_z, &gyro_x, &gyro_y, &gyro_z, &temperature);
+        xSemaphoreGive(g_xMutex_Sensor); // Å£Å£Å£ End critical section Å£Å£Å£
+    	if(error == 0) {
+            char txt[128];
+            sprintf(txt, "  Acceleration = (%f, %f, %f)", axl_x, axl_y, axl_z);
+    		Serial.println(txt);
+            sprintf(txt, "  Gyro = (%f, %f, %f)", gyro_x, gyro_y, gyro_z);
+    		Serial.println(txt);
+            sprintf(txt, "  Temperature = %f", temperature);
+    		Serial.println(txt);
+        }
+    	else {
+    		Serial.println("[Error] cannot read MPU-6050.");
+    	}
+    }
+    
+    if(g_rpr0521rs_enable) { // RPR-0521RS was initialized
+    	unsigned short ps_val;
+    	float als_val;
+        xSemaphoreTake(g_xMutex_Sensor, portMAX_DELAY); // Å•Å•Å• Start critical section Å•Å•Å•
+    	error = rpr0521rs.get_psalsval(&ps_val, &als_val);
+        xSemaphoreGive(g_xMutex_Sensor); // Å£Å£Å£ End critical section Å£Å£Å£
+    	if(error == 0) {
+    		// Proximity
+    		Serial.print("  Proximity = ");
+    		Serial.println(ps_val);
+    		// Brightness
+    		Serial.print("  Brightness = ");
+    		Serial.println(als_val);
+
+    		Serial.println();
+    	}
+    	else {
+    		Serial.println("[Error] cannot read RPR-0521RS.");
+    	}
+    }
+}
+
 //===================================================================
 // Command table
 //===================================================================
@@ -218,6 +311,9 @@ T_command_info  g_command_table[] = {
     {"serial",      _cmd__serial},
     {"speed",       _cmd__speed},
     {"signal",      _cmd__signal},
+    {"brightth",    _cmd__brightth},
+    {"impactth",    _cmd__impactth},
+    {"sensor",      _cmd__sensor},
     // The last line must be NULL
     {NULL,          NULL},
 };
@@ -454,11 +550,9 @@ int create_event()
     static int is_first = 1;
     static int pre_signal_color;
 
-    xSemaphoreTake(g_xMutex_Signal, portMAX_DELAY);
-    // Å•Å•Å• Start critical section Å•Å•Å•
+    xSemaphoreTake(g_xMutex_Signal, portMAX_DELAY); // Å•Å•Å• Start critical section Å•Å•Å•
     int signal_color = g_trafic_signal_color;
-    // Å£Å£Å£ End critical section Å£Å£Å£
-    xSemaphoreGive(g_xMutex_Signal);
+    xSemaphoreGive(g_xMutex_Signal); // Å£Å£Å£ End critical section Å£Å£Å£
     if(is_first) {
         pre_signal_color = signal_color;
         is_first = 0;
@@ -515,6 +609,7 @@ int get_next_state(int cur_state, int event)
 void Task_line_trace(void* param)
 {
     xSemaphoreGive(g_xMutex_Signal);
+    xSemaphoreGive(g_xMutex_Sensor);
     for(;;) {
         vTaskDelay(10);
 
@@ -551,20 +646,14 @@ void Task_line_trace(void* param)
 void Task_serial_cmd(void* param)
 {
     int     cmd_mode = CMD_MODE_LINE;
-    char    command_line[256];
+    char    command_line[128];
     int     cmd_index = 0;
 
     xSemaphoreGive(g_xMutex_Signal);
+    xSemaphoreGive(g_xMutex_Sensor);
     for(;;) {
         vTaskDelay(10);
         
-        if(g_button1.get_status()) {
-            g_led1.set_status(1);
-        }
-        else {
-            g_led1.set_status(0);
-        }
-
         if(g_pSerial->available() > 0) { // received data
             char getstr = g_pSerial->read(); // Read data from serial-port
             if(cmd_mode == CMD_MODE_LINE) {
@@ -573,16 +662,19 @@ void Task_serial_cmd(void* param)
                 if(getstr == '\n') { // Detect "LF"(enter key)
                     command_line[cmd_index-1] = '\0';
                     cmd_index = 0;
-                    
+
                     // "key" command
-                    char* p = strtok(command_line, " ");
+                    char    temp[8];
+                    strncpy(temp, command_line, 7);
+                    temp[7] = '\0';
+                    char* p = strtok(temp, " ");
                     if(p) {
                         if(strcmp(p, "key") == 0) {
                             g_pSerial->println("Changed to Key command mode.");
                             cmd_mode = CMD_MODE_KEY;
                         }
                     }
-                    
+
                     // Parse and execute command
                     parse_and_exec_cmd(command_line, g_command_table);
                 }
@@ -608,19 +700,49 @@ void Task_serial_cmd(void* param)
 //===================================================================
 void Task_sensor(void* param)
 {
+    xSemaphoreGive(g_xMutex_Signal);
+    xSemaphoreGive(g_xMutex_Sensor);
+	byte rc;
+	float pre_abs_axl = 0;
+	portTickType wakeupTime = xTaskGetTickCount();
     for(;;) {
-        vTaskDelay(1000);
+        vTaskDelayUntil(&wakeupTime, 50);
+
+        if(g_mpu6050_enable) { // RPR-0521RS was initialized
+    		// Get acceleration and temperature
+        	float	axl_x, axl_y, axl_z;
+        	float	gyro_x, gyro_y, gyro_z;
+        	float	temperature;
+            xSemaphoreTake(g_xMutex_Sensor, portMAX_DELAY); // Å•Å•Å• Start critical section Å•Å•Å•
+    		int error = MPU6050_get_all(&axl_x, &axl_y, &axl_z, &gyro_x, &gyro_y, &gyro_z, &temperature);
+            xSemaphoreGive(g_xMutex_Sensor); // Å£Å£Å£ End critical section Å£Å£Å£
+    		// Detect impact
+    		float abs_axl = (axl_x*axl_x + axl_y*axl_y + axl_z*axl_z);
+    		float diff_axl = abs_axl - pre_abs_axl;
+    		if(diff_axl < 0) { // abs
+    			diff_axl = -diff_axl;
+    		}
+    		pre_abs_axl = abs_axl;
+            if(diff_axl < g_impact_thresh) {
+                g_led1.turn(0);
+            }
+            else {
+                g_led1.turn(1);
+            }
+                
+        }
 
         if(g_rpr0521rs_enable) { // RPR-0521RS was initialized
             unsigned short ps_val;
             float als_val;
+            xSemaphoreTake(g_xMutex_Sensor, portMAX_DELAY); // Å•Å•Å• Start critical section Å•Å•Å•
             rpr0521rs.get_psalsval(&ps_val, &als_val);
-
+            xSemaphoreGive(g_xMutex_Sensor); // Å£Å£Å£ End critical section Å£Å£Å£
             if(als_val > g_brightness_thresh) {
-                g_led1.set_status(1);
+                g_led2.turn(1);
             }
             else {
-                g_led1.set_status(0);
+                g_led2.turn(0);
             }
         }
     }
@@ -638,9 +760,18 @@ void setup()
     // Initialize Bluetooth serial
     SerialBT.begin("ESP32-12135x");
 
+	Wire.begin(SDA, SCL);
+
+    // Initialize RPR-0521RS
     byte rc = rpr0521rs.init();
     if(rc == 0) {
         g_rpr0521rs_enable = 1;
+    }
+
+    // Initialize MPU-6050
+	int err = MPU6050_init(&Wire);
+    if(err == 0) {
+        g_mpu6050_enable = 1;
     }
 
     // Initialize motor
@@ -651,14 +782,15 @@ void setup()
     pinMode(IO_PIN_LINETRACK_CENTER, INPUT);
     pinMode(IO_PIN_LINETRACK_RIGHT, INPUT);
 
-    // Create FreeRTOS tasks
-    xTaskCreatePinnedToCore(Task_line_trace, "Task_line_trace", 2048, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(Task_serial_cmd, "Task_serial_cmd", 2048, NULL, 1, NULL, 0);
-    xTaskCreatePinnedToCore(Task_sensor, "Task_sensor", 2048, NULL, 3, NULL, 0);
-    
     // Create semaphore
     g_xMutex_Signal = xSemaphoreCreateMutex();
+    g_xMutex_Sensor = xSemaphoreCreateMutex();
 
+    // Create FreeRTOS tasks
+    xTaskCreatePinnedToCore(Task_line_trace, "Task_line_trace", 2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(Task_serial_cmd, "Task_serial_cmd", 4096, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(Task_sensor, "Task_sensor", 2048, NULL, 3, NULL, 0);
+    
     // Go forward
     RoboCar_stop();
 }
