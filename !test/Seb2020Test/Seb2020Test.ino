2ab9d1569e51a3ec36c373948adf5e38d881484a
nishimura <yushi.nishimura@konicaminolta.com>
2020-05-30 21:59:55 +0900

    temp

--- !test/Seb2020Test/Seb2020Test.ino
+++ !test/Seb2020Test/Seb2020Test.ino
@@ -18,24 +18,72 @@
  THE SOFTWARE.
 ******************************************************************************/
 #include <Wire.h>
+#include <my_led_button.h>
 #include <RPR-0521RS.h>
 
-RPR0521RS rpr0521rs;
+// BMX055　加速度センサのI2Cアドレス  
+#define Addr_Accl 0x19  // (JP1,JP2,JP3 = Openの時)
+// BMX055　ジャイロセンサのI2Cアドレス
+#define Addr_Gyro 0x69  // (JP1,JP2,JP3 = Openの時)
+// BMX055　磁気センサのI2Cアドレス
+#define Addr_Mag 0x13   // (JP1,JP2,JP3 = Openの時)
+
+
+//---------------------- PIN ----------------------
+#define PIN_BUTTON          (0) // 
+#define PIN_LED_01          (2)
+#define PIN_LED_RED         (13)
+#define PIN_LED_YELLOW      (12)
+#define PIN_LED_GREEN       (14)
+#define PIN_LED_WHITE       (23)
 
 #define PIN_SDA SDA
 #define PIN_SCL SCL
+
+//---------------------- D/A converter channel ----------------------
+#define DAC_CH_LED_01           (3)
+#define DAC_CH_LED_RED          (4)
+#define DAC_CH_LED_YELLOW       (5)
+#define DAC_CH_LED_GREEN        (6)
+#define DAC_CH_LED_WHITE        (7)
+
+// センサーの値を保存するグローバル関数
+float xAccl = 0.00;
+float yAccl = 0.00;
+float zAccl = 0.00;
+float xGyro = 0.00;
+float yGyro = 0.00;
+float zGyro = 0.00;
+int   xMag  = 0;
+int   yMag  = 0;
+int   zMag  = 0;
+
+
+my_led g_led_01(PIN_LED_01, DAC_CH_LED_01);
+my_led g_led_red(PIN_LED_RED, DAC_CH_LED_RED);
+my_led g_led_yellow(PIN_LED_YELLOW, DAC_CH_LED_YELLOW);
+my_led g_led_green(PIN_LED_GREEN, DAC_CH_LED_GREEN);
+my_led g_led_white(PIN_LED_WHITE, DAC_CH_LED_WHITE);
+
+RPR0521RS rpr0521rs;
+
 
 
 void setup() 
 {
 	byte rc;
 
-	Serial.begin(9600);
+	Serial.begin(115200);
 	while (!Serial);
 
 	Wire.begin(PIN_SDA, PIN_SCL);
+	
+    pinMode(PIN_BUTTON, INPUT);
 
 	rc = rpr0521rs.init();
+
+    //BMX055 初期化
+    BMX055_Init();
 }
 
 void loop() 
@@ -44,13 +92,23 @@ void loop()
 	unsigned short ps_val;
 	float als_val;
 
+    // Get sensor val from RPR-0521
 	error = rpr0521rs.get_psalsval(&ps_val, &als_val);
 	if(error == 0) {
-		// �ߐڃZ���T�[
+		// 近接センサー
 		Serial.print("Prox. = ");
 		Serial.print(ps_val);
 		Serial.print("\t||\t");
-		// �Ɠx�Z���T�[
+		uint32_t br = (int)(ps_val/10);
+		if(ps_val == 0) {
+            g_led_white.turn(0);
+        }
+        else {
+            g_led_white.turn(1);
+            g_led_white.set_brightness(br);
+        }
+		
+		// 照度センサー
 		Serial.print("Bright. = ");
 		Serial.print(als_val);
 
@@ -60,5 +118,154 @@ void loop()
 		Serial.println("[Error] cannot get sensor data.");
 	}
 
-	delay(1000);
+	delay(100);
+}
+
+//=====================================================================================//
+void BMX055_Init()
+{
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x0F); // Select PMU_Range register
+  Wire.write(0x03);   // Range = +/- 2g
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x10);  // Select PMU_BW register
+  Wire.write(0x08);  // Bandwidth = 7.81 Hz
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x11);  // Select PMU_LPW register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 0.5ms
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x0F);  // Select Range register
+  Wire.write(0x04);  // Full scale = +/- 125 degree/s
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x10);  // Select Bandwidth register
+  Wire.write(0x07);  // ODR = 100 Hz
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x11);  // Select LPM1 register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 2ms
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x83);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x01);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4C);  // Select Mag register
+  Wire.write(0x00);  // Normal Mode, ODR = 10 Hz
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4E);  // Select Mag register
+  Wire.write(0x84);  // X, Y, Z-Axis enabled
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x51);  // Select Mag register
+  Wire.write(0x04);  // No. of Repetitions for X-Y Axis = 9
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x52);  // Select Mag register
+  Wire.write(0x16);  // No. of Repetitions for Z-Axis = 15
+  Wire.endTransmission();
+}
+//=====================================================================================//
+void BMX055_Accl()
+{
+  int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Accl);
+    Wire.write((2 + i));// Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Accl, 1);// Request 1 byte of data
+    // Read 6 bytes of data
+    // xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data to 12-bits
+  xAccl = ((data[1] * 256) + (data[0] & 0xF0)) / 16;
+  if (xAccl > 2047)  xAccl -= 4096;
+  yAccl = ((data[3] * 256) + (data[2] & 0xF0)) / 16;
+  if (yAccl > 2047)  yAccl -= 4096;
+  zAccl = ((data[5] * 256) + (data[4] & 0xF0)) / 16;
+  if (zAccl > 2047)  zAccl -= 4096;
+  xAccl = xAccl * 0.0098; // renge +-2g
+  yAccl = yAccl * 0.0098; // renge +-2g
+  zAccl = zAccl * 0.0098; // renge +-2g
+}
+//=====================================================================================//
+void BMX055_Gyro()
+{
+  int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Gyro);
+    Wire.write((2 + i));    // Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Gyro, 1);    // Request 1 byte of data
+    // Read 6 bytes of data
+    // xGyro lsb, xGyro msb, yGyro lsb, yGyro msb, zGyro lsb, zGyro msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data
+  xGyro = (data[1] * 256) + data[0];
+  if (xGyro > 32767)  xGyro -= 65536;
+  yGyro = (data[3] * 256) + data[2];
+  if (yGyro > 32767)  yGyro -= 65536;
+  zGyro = (data[5] * 256) + data[4];
+  if (zGyro > 32767)  zGyro -= 65536;
+
+  xGyro = xGyro * 0.0038; //  Full scale = +/- 125 degree/s
+  yGyro = yGyro * 0.0038; //  Full scale = +/- 125 degree/s
+  zGyro = zGyro * 0.0038; //  Full scale = +/- 125 degree/s
+}
+//=====================================================================================//
+void BMX055_Mag()
+{
+  int data[8];
+  for (int i = 0; i < 8; i++)
+  {
+    Wire.beginTransmission(Addr_Mag);
+    Wire.write((0x42 + i));    // Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Mag, 1);    // Request 1 byte of data
+    // Read 6 bytes of data
+    // xMag lsb, xMag msb, yMag lsb, yMag msb, zMag lsb, zMag msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data
+  xMag = ((data[1] <<8) | (data[0]>>3));
+  if (xMag > 4095)  xMag -= 8192;
+  yMag = ((data[3] <<8) | (data[2]>>3));
+  if (yMag > 4095)  yMag -= 8192;
+  zMag = ((data[5] <<8) | (data[4]>>3));
+  if (zMag > 16383)  zMag -= 32768;
 }
