fbb416adc3f1ee5e18d007f472a77db31163dadb
nishimura <yushi.nishimura@konicaminolta.com>
2020-05-31 18:30:43 +0900

    Ambientへ送信

--- !test/DHT11Test/DHT11Test.ino
+++ !test/DHT11Test/DHT11Test.ino
@@ -1,23 +1,44 @@
+#include <WiFi.h>
 #include "DHT.h"
+#include <Ambient.h>
+
+const char* ssid = "HUMAX-FD2A6";
+const char* password = "XTk4NWN4NTM4X";
+
+WiFiClient client;
+Ambient ambient;
 
 DHT dht(33,DHT11);  //2番ポート
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("DHT11 trial");
-  dht.begin();
+unsigned int channelId = 20947; // AmbientのチャネルID
+const char* writeKey = "10cdb0759b1a687e"; // ライトキー
 
+void setup() {
+    Serial.begin(115200);
+    while (!Serial) ;
+
+    WiFi.begin(ssid, password);  // Wi-Fiネットワークに接続する
+    while (WiFi.status() != WL_CONNECTED) {  // 接続したか調べる
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());  // ローカルIPアドレスをプリントする
+
+    ambient.begin(channelId, writeKey, &client); // チャネルIDとライトキーを指定してAmbientの初期化
+
+    dht.begin();
 }
 
 void loop() {
+    float temp = dht.readHumidity();
+    float humid = dht.readTemperature();
+    Serial.printf("temp: %.2f, humid: %.2f\r\n", temp, humid);
 
-  delay(3000);
+    ambient.set(1, temp);  // Ambientのデータ1に温度をセットする
+    ambient.set(2, humid);  // データ2に湿度をセットする
+    ambient.send();  // Ambientに送信する
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  Serial.print("Humidity : ");
-  Serial.println(h);
-  Serial.print("Temperature : ");
-  Serial.println(t);
+    delay(300 * 1000);
 }
